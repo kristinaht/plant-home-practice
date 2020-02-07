cae1b498700eed82737c1cfe273d091e058ea220
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-02-07 15:04:30 -0800

    run tests again to make sure all other behaviors still work after modifying the first spec

--- __test__/age-calculator.test.js
+++ __test__/age-calculator.test.js
@@ -6,22 +6,22 @@ describe ("User", () => {
     expect(user.age).toEqual(35);
     expect(user.lifeExpectancy).toEqual(90);
   }); 
-  // test("should correctly calculate user age in Mercury years", () => {
-  //   let newUser = new User(35);
-  //   expect(newUser.getMercuryAge()).toEqual(146);
-  // });
-  // test("should correctly calculate user's age in Venus years", () => {
-  //   let newUser = new User(35);
-  //   expect(newUser.getVenusAge()).toEqual(56);
-  // });
-  // test("should correctly calculate user age in Mars years", () => {
-  //   let newUser = new User(35);
-  //   expect(newUser.getMarsAge()).toEqual(19);
-  // });
-  // test("should correctly calculate age in Jupiter years", () => {
-  //   let newUser = new User(35);
-  //   expect(newUser.getJupiterAge()).toEqual(3);
-  // });
+  test("should correctly calculate user age in Mercury years", () => {
+    let newUser = new User(35);
+    expect(newUser.getMercuryAge()).toEqual(146);
+  });
+  test("should correctly calculate user's age in Venus years", () => {
+    let newUser = new User(35);
+    expect(newUser.getVenusAge()).toEqual(56);
+  });
+  test("should correctly calculate user age in Mars years", () => {
+    let newUser = new User(35);
+    expect(newUser.getMarsAge()).toEqual(19);
+  });
+  test("should correctly calculate age in Jupiter years", () => {
+    let newUser = new User(35);
+    expect(newUser.getJupiterAge()).toEqual(3);
+  });
   // test("should correctly calculate life expectancy on Mercury")
   //   let newUser = new User(35, 90);
   //   expect(newUser.getMercuryLifeExpectancy()).toEqual(229);
